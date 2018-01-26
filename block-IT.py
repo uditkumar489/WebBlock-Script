c41cc44ffd1d79dd3525765122b0795aa91c44ef
udit kumar <uditkumar489@gmail.com>
2018-01-26 07:10:03 +0530

    done !!
    ..

--- block-IT.py
+++ block-IT.py
@@ -1,11 +1,11 @@
 import time
 from datetime import datetime as dt
 
-hosts_path_windows = "C:\Windows\System32\drivers\etc\hosts"
+hosts_path_windows = "C:\Windows\System32\drivers\etc\hosts" # This is for WINDOWS-users only || check READme.md for mac and unix
 
 redirect = "127.0.0.1"
 
-websites_to_block = ["www.pointless.com","www.pointlesssites.com/"]  
+websites_to_block = ["www.pointless.com","www.pointlesssites.com/"]  # Replace with your own
 
 count = 0
 
