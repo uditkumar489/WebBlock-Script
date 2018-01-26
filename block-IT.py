d078c48cc9fb2f3bcf252a775d81e883a58ee105
udit kumar <uditkumar489@gmail.com>
2018-01-26 07:08:16 +0530

    added sleep on sample web_S
    ..

--- block-IT.py
+++ block-IT.py
@@ -3,3 +3,23 @@ from datetime import datetime as dt
 
 hosts_path_windows = "C:\Windows\System32\drivers\etc\hosts"
 
+redirect = "127.0.0.1"
+
+websites_to_block = ["www.pointless.com","www.pointlesssites.com/"]  
+
+count = 0
+
+while True:
+    
+    with open(hosts_path_windows,'r+') as file:
+        
+        content = file.read()
+        
+        for website in websites_to_block:
+            
+            if website in content:
+                pass
+            else:
+                file.write(redirect + " " + website + "\n")
+                
+    time.sleep(5)
